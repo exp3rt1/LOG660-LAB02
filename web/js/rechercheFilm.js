6b7642f2c0bcad0591747483e583210bfd6bfcdf
olivier <oli_9410@hotmail.com>
2016-10-07 12:37:02 -0400

    Improved table view and add javascript file

---
+++ web/js/rechercheFilm.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+    
+    $('#rechercheBouton').click(function(){
+        
+    });
+    
+    $('#rechercheAvanceeBouton').click(function(){
+        $('#titres').val();
+        $('#realisateurs').val();
+        $('#acteurs').val();
+        $('#debutAnnee').val();
+        $('#finAnnee').val();
+        $('#pays').val();
+        $('#languesOriginales').val();
+        $('#genres').val();
+    });
+});
