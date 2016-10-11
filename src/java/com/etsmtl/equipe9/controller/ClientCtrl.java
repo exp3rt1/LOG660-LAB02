a865d24aa1f89c672e3945acdf957f2523707372
olivier <oli_9410@hotmail.com>
2016-10-11 19:58:37 -0400

    First call with WS

--- src/java/com/etsmtl/equipe9/controller/ClientCtrl.java
+++ src/java/com/etsmtl/equipe9/controller/ClientCtrl.java
@@ -6,15 +6,20 @@ import com.etsmtl.equipe9.dao.ClientDAO;
 
 public class ClientCtrl {
     
-    public String getPassword (String courriel){
-        
+    public boolean getPassword (String courriel, String motDePasse){
+        // Quand on va avoir hasher le mot de passe, il faudra le dehasher
         ClientDAO dao = new ClientDAO();
-        return dao.findById(courriel).getMotpasse();
+        String motDePasseBD = dao.findById(courriel).getMotpasse();
+        
+        if (motDePasse.equals(motDePasseBD)){
+            return true;
+        }
+        return false;
     }
     
     public static void main(String[] args) {
-        ClientCtrl pass = new ClientCtrl();
-        String password = pass.getPassword("RobertBSutton56@yahoo.com");
-        System.out.println("Le password est: "+ password);
+        //ClientCtrl pass = new ClientCtrl();
+        //String password = pass.getPassword("RobertBSutton56@yahoo.com");
+        //System.out.println("Le password est: "+ password);
     }
 }
