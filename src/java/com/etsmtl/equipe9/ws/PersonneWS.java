79f501888fdd6e57df6de39bffcc162d2f62d1d1
olivier <oli_9410@hotmail.com>
2016-10-14 14:58:28 -0400

    start personne ws

--- src/java/com/etsmtl/equipe9/ws/PersonneWS.java
+++ src/java/com/etsmtl/equipe9/ws/PersonneWS.java
@@ -1,12 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.etsmtl.equipe9.ws;
 
 import com.etsmtl.equipe9.controller.ClientCtrl;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ejb.Stateless;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -14,31 +14,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-
-@WebServlet("/login")
-public class ClientWS extends HttpServlet {
+/**
+ *
+ * @author Oli
+ */
+@WebServlet("/personne/{id}")
+public class PersonneWS extends HttpServlet {
     private ClientCtrl ctrl = new ClientCtrl();
 	
     @Context
     private UriInfo context;
     
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
         Cookie idCookie;
         HttpSession session;
